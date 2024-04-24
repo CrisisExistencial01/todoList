7de63a195a8a707ece384c099df11683cd555736
Cris <c.cheuquel01@ufromail.cl>
2024-04-24 18:19:03 -0400

    actividad 24 de abril

--- simpleDatos.py
+++ simpleDatos.py
@@ -1,12 +1,24 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+data_list = []
+# data = {
+#   {"item": item , "cantidad": cantidad},
+# }
+@app.route('/items', methods=['GET'])
+def get_items():
+    
+    return jsonify(data_list)
 
-@app.route('/data')
-def get_data():
-    data = {'name': 'John', 'age': 30, 'city': 'New York'}
-    return jsonify(data)
+@app.route('/add_item', methods=['POST'])
+def add_item():
+
+    new_data = request.get_json()
+
+    data_list.append(new_data)
+
+    return jsonify({'message': 'Data added successfully'}), 201
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8081)
 
